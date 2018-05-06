0f96916eed737bb48ce01d6ef9632bd7c6549f04
bowenislandsong <bowenislandsong@gmail.com>
2018-05-06 11:02:50 -0400

    polish done

--- mongoDB/phase3/phase3.py
+++ mongoDB/phase3/phase3.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import sys
 sys.path.insert(0, "../../Assignment1/")
+sys.path.insert(1, "../Phase2/")
 import twmager
 import img2video
 import vision
 import json
 import mogoLog as lg
-import trend
 import glob
-import trend
+import compare
 
-
-
+screenname = "etcwilde"
 with open('twittersecrete.json') as secret:
 	Tsecret = json.load(secret)
 
@@ -21,10 +20,10 @@ with open('twittersecrete.json') as secret:
 # allow users to scan all the friends
 twmager.twitter_api(Tsecret['Consumer_Key'], Tsecret['Consumer_Secret'],Tsecret['Access_Token'], Tsecret['Access_Token_Secret'])
 
-twmager.download_tw_img("etcwilde")
+twmager.download_tw_img(screenname)
 # 
 
-cli = lg.loguser("mongodb://localhost:27017","etcwilde")
+cli = lg.loguser("mongodb://localhost:27017",screenname)
 # client.drop_database('<DBNAME>')
 
 for infile in glob.glob("*.jpg"):
@@ -34,13 +33,14 @@ for infile in glob.glob("*.jpg"):
 		for t in text:
 			lg.loginterests(t)
 
-list = g.checktop(5)
+lg.savetop(20)
 
-lg.uploadlog()
+
+top5 = lg.uploadlog()
 
 
 img2video.jpg2mp4()
 
 img2video.rmPWDImg()
 
-trend.compare(list)
+compare.showcompare(screenname,top5)
